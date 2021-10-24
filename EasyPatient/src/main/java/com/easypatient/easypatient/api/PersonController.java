40dcc41f596d00002a99b9326fb651ed6afc6091
rafalkrol100 <rafalkrol100@gmail.com>
2021-10-24 23:50:44 +0200

    feature/BD-002 Utworzenie OpenAPI
    *zmiaony stylistyczne w endpoitach

--- EasyPatient/src/main/java/com/easypatient/easypatient/api/PersonController.java
+++ EasyPatient/src/main/java/com/easypatient/easypatient/api/PersonController.java
@@ -3,6 +3,7 @@ package com.easypatient.easypatient.api;
 import com.easypatient.easypatient.model.Person;
 import com.easypatient.easypatient.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,18 +31,20 @@ public class PersonController {
         return personService.getAllPeople();
     }
 
-    @GetMapping(path = "{id}")
+    @GetMapping("/{id}")
     public Person getPersonById(@PathVariable("id") UUID id) {
         return personService.getPersonById(id)
                 .orElse(null);
     }
 
-    @DeleteMapping(path = "{id}")
+    @DeleteMapping(path = "/{id}")
     public void deletePersonById(@PathVariable("id") UUID id) {
         personService.deletePerson(id);
     }
 
-    @PutMapping(path = "{id}")
+    @PutMapping(path = "/{id}",
+    consumes={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+    produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE} )
     public void updatePerson(@PathVariable("id") UUID id, @Valid @NonNull @RequestBody Person PersonToUpdate) {
         personService.updatePerson(id, PersonToUpdate);
     }
