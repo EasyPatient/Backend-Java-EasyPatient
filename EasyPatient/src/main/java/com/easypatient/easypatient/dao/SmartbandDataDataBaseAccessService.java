c54e6268f72c40f964823b148e25d7098ad15eb7
rafal <rafalrobertkolacz@gmail.com>
2021-11-21 16:16:29 +0100

    feature/BD-007 Implementacja klas DataBaseAccessService
    *Dodanie metod insert

--- EasyPatient/src/main/java/com/easypatient/easypatient/dao/SmartbandDataDataBaseAccessService.java
+++ EasyPatient/src/main/java/com/easypatient/easypatient/dao/SmartbandDataDataBaseAccessService.java
@@ -2,16 +2,42 @@ package com.easypatient.easypatient.dao;
 
 import com.easypatient.easypatient.dto.SmartbandDataDTO;
 import com.easypatient.easypatient.dto.SmartbandDataGetDTO;
+import com.easypatient.easypatient.model.Smartband;
+import com.easypatient.easypatient.model.SmartbandData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Repository("SmartbandDataPostgres")
 public class SmartbandDataDataBaseAccessService implements SmartbandDataDao {
+
+    final String sqlInsertSmartbandData = "INSERT INTO patient VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public SmartbandDataDataBaseAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public void insertSmartbandData(SmartbandDataDTO smartbandData) {
+        LocalDateTime date = LocalDateTime.now();
+
+        jdbcTemplate.update(sqlInsertSmartbandData,
+                smartbandData.getSmartbandId(),
+                smartbandData.getPatientId(),
+                smartbandData.getHeartRate(),
+                smartbandData.getOxygen(),
+                smartbandData.getTemperature(),
+                smartbandData.getBattery(),
+                date,
+                date);
     }
 
     @Override
--- EasyPatient/src/main/java/com/easypatient/easypatient/dao/StaffDataBaseAccessService.java
+++ EasyPatient/src/main/java/com/easypatient/easypatient/dao/StaffDataBaseAccessService.java
@@ -2,6 +2,8 @@ package com.easypatient.easypatient.dao;
 
 import com.easypatient.easypatient.dto.StaffDTO;
 import com.easypatient.easypatient.dto.StaffGetDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
@@ -12,8 +14,30 @@ import java.util.UUID;
 
 @Repository("StaffPostgres")
 public class StaffDataBaseAccessService implements StaffDao {
+
+    final String sqlInsertPatient = "INSERT INTO patient VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public StaffDataBaseAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
     @Override
     public void insertStaff(StaffDTO staff) {
+        LocalDateTime date = LocalDateTime.now();
+
+        jdbcTemplate.update(sqlInsertPatient,
+                staff.getName(),
+                staff.getEmail(),
+                staff.getPhone(),
+                staff.getPhoneAreaCode(),
+                staff.getPassword(),
+                staff.getRole(),
+                date,
+                date);
     }
 
     @Override
