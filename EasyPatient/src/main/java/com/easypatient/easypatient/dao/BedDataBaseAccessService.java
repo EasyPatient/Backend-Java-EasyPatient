bba016080877ea140fc164a507a29518593eb3b8
rafalkrol100 <rafalkrol100@gmail.com>
2021-11-21 17:30:57 +0100

    [feature/BD-008] Implementacja klas
    *implementacja gertbyvariables w bed

--- EasyPatient/src/main/java/com/easypatient/easypatient/dao/BedDataBaseAccessService.java
+++ EasyPatient/src/main/java/com/easypatient/easypatient/dao/BedDataBaseAccessService.java
@@ -44,6 +44,112 @@ public class BedDataBaseAccessService implements BedDao {
                                                 Optional<UUID> roomId,
                                                 Optional<LocalDateTime> updatedAt,
                                                 Optional<LocalDateTime> createdAt) throws SQLException {
-        return List.of();
+        int i = 0;
+        int k = 0;
+        final String sqlSelectBedByVariable = "SELECT id, number, patient_id, room_id, created_at, updated_at FROM bed WHERE ";
+        final String sqlNumber = " (number = ?)";
+        final String sqlPatientId = " (patient_id = ?)";
+        final String sqlRoomId = " (room_id = ?)";
+        final String sqlUpdatedAt = " (created_at = ?)";
+        final String sqlCreatedAt = " (updated_at = ?)";
+        final String sqlAnd = " AND ";
+        final String sqlSemicolon = ";";
+
+        List<String> expressions = new java.util.ArrayList<>(List.of(sqlSelectBedByVariable));
+
+        if(number.isPresent()) {
+            i++;
+            expressions.add(sqlNumber);
+        }
+        if(patientId.isPresent()) {
+            i++;
+            expressions.add(sqlPatientId);
+            if(i > 1) {
+                expressions.add(sqlAnd);
+            }
+        }
+        if(roomId.isPresent()) {
+            i++;
+            expressions.add(sqlRoomId);
+            if(i > 1) {
+                expressions.add(sqlAnd);
+            }
+        }
+        if(updatedAt.isPresent()) {
+            i++;
+            expressions.add(sqlUpdatedAt);
+            if(i > 1) {
+                expressions.add(sqlAnd);
+            }
+        }
+        if(createdAt.isPresent()) {
+            i++;
+            expressions.add(sqlCreatedAt);
+            if(i > 1) {
+                expressions.add(sqlAnd);
+            }
+        }
+
+        expressions.add(sqlSemicolon);
+        String sqlExpression = String.join(" ", expressions);
+
+        if(i != 0) {
+            Object[] jdbcTable = new Object[i];
+            if(number.isPresent()) {
+                jdbcTable[k] = number.get();
+                k++;
+            }
+            if(patientId.isPresent()) {
+                jdbcTable[k] = patientId.get();
+                k++;
+            }
+            if(roomId.isPresent()) {
+                jdbcTable[k] = roomId.get();
+                k++;
+            }
+            if(updatedAt.isPresent()) {
+                jdbcTable[k] = updatedAt.get();
+                k++;
+            }
+            if(createdAt.isPresent()) {
+                jdbcTable[k] = createdAt.get();
+                k++;
+            }
+            return jdbcTemplate.query(
+                    sqlExpression,
+                    jdbcTable,
+                    BedDataBaseAccessService::mapRow);
+        } else {
+            throw new SQLException("can not query without any parameters!");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
